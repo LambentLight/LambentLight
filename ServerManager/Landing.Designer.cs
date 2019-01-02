a57d766c748b283d1358fb8ec1aaf4ad2b0f7695
Hannele Ruiz <justlemoncl@gmail.com>
2019-01-02 01:44:09 -0300

    Renaming the title of the window from "Form1" to "ServerManager"

--- ServerManager/Landing.Designer.cs
+++ ServerManager/Landing.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Landing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Landing";
+            this.Text = "ServerManager";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
