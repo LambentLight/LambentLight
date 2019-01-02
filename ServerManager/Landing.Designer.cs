57f676b86c4ba96972642a643fc2e4b70ee58e40
Hannele Ruiz <justlemoncl@gmail.com>
2019-01-02 02:03:24 -0300

    Added section for stdin, stdout and stderr

--- ServerManager/Landing.Designer.cs
+++ ServerManager/Landing.Designer.cs
@@ -34,8 +34,12 @@
             this.RefreshBuilds = new System.Windows.Forms.Button();
             this.DataList = new System.Windows.Forms.ComboBox();
             this.RefreshData = new System.Windows.Forms.Button();
+            this.Console = new System.Windows.Forms.GroupBox();
+            this.ServerOutput = new System.Windows.Forms.TextBox();
+            this.ServerInput = new System.Windows.Forms.TextBox();
             this.BuildSelector.SuspendLayout();
             this.ServerData.SuspendLayout();
+            this.Console.SuspendLayout();
             this.SuspendLayout();
             // 
             // BuildSelector
@@ -96,17 +100,46 @@
             this.RefreshData.Text = "Refresh List";
             this.RefreshData.UseVisualStyleBackColor = true;
             // 
+            // Console
+            // 
+            this.Console.Controls.Add(this.ServerInput);
+            this.Console.Controls.Add(this.ServerOutput);
+            this.Console.Location = new System.Drawing.Point(12, 68);
+            this.Console.Name = "Console";
+            this.Console.Size = new System.Drawing.Size(776, 370);
+            this.Console.TabIndex = 2;
+            this.Console.TabStop = false;
+            this.Console.Text = "Console";
+            // 
+            // ServerOutput
+            // 
+            this.ServerOutput.Location = new System.Drawing.Point(6, 19);
+            this.ServerOutput.Multiline = true;
+            this.ServerOutput.Name = "ServerOutput";
+            this.ServerOutput.Size = new System.Drawing.Size(764, 319);
+            this.ServerOutput.TabIndex = 0;
+            // 
+            // ServerInput
+            // 
+            this.ServerInput.Location = new System.Drawing.Point(6, 344);
+            this.ServerInput.Name = "ServerInput";
+            this.ServerInput.Size = new System.Drawing.Size(764, 20);
+            this.ServerInput.TabIndex = 1;
+            // 
             // Landing
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Console);
             this.Controls.Add(this.ServerData);
             this.Controls.Add(this.BuildSelector);
             this.Name = "Landing";
             this.Text = "ServerManager";
             this.BuildSelector.ResumeLayout(false);
             this.ServerData.ResumeLayout(false);
+            this.Console.ResumeLayout(false);
+            this.Console.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -119,6 +152,9 @@
         private System.Windows.Forms.Button RefreshBuilds;
         private System.Windows.Forms.Button RefreshData;
         private System.Windows.Forms.ComboBox DataList;
+        private System.Windows.Forms.GroupBox Console;
+        private System.Windows.Forms.TextBox ServerInput;
+        private System.Windows.Forms.TextBox ServerOutput;
     }
 }
 
