5f580ffb29e960be91f18f4bc0d10b9d77deb074
Hannele Ruiz <justlemoncl@gmail.com>
2019-01-02 18:40:11 -0300

    Added TextBoxBase extension to automatically add line endings

---
+++ ServerManager/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ServerManager
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Appends a line into the TextBox.
+        /// </summary>
+        /// <param name="Line">The text to add at the end.</param>
+        public static void AppendLine(this TextBoxBase Box, string Line)
+        {
+            Box.AppendText(Line);
+            Box.AppendText(Environment.NewLine);
+        }
+    }
+}
