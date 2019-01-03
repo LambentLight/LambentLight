5df8ac73557c5e78f8a879c967f3a95cb6dc6851
Hannele Ruiz <justlemoncl@gmail.com>
2019-01-03 19:39:38 -0300

    Added enum to guide the current server status

---
+++ ServerManager/Enums.cs
@@ -0,0 +1,11 @@
+﻿namespace ServerManager
+{
+    /// <summary>
+    /// Current server status.
+    /// </summary>
+    public enum Status
+    {
+        Stopped = 0,
+        Running = 1
+    }
+}
