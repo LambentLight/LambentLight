4eedbd9ac3159124a342f2264d63b0d752f4bc0a
Hannele Ruiz <justlemoncl@gmail.com>
2019-07-20 11:11:14 -0400

    Added group box for the Builds and Data folders

--- LambentLight/Landing.Designer.cs
+++ LambentLight/Landing.Designer.cs
@@ -28,21 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.BuildsGroup = new System.Windows.Forms.GroupBox();
+            this.BuildsBox = new System.Windows.Forms.ComboBox();
+            this.DataGroup = new System.Windows.Forms.GroupBox();
+            this.DataBox = new System.Windows.Forms.ComboBox();
+            this.BuildsGroup.SuspendLayout();
+            this.DataGroup.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // BuildsGroup
+            // 
+            this.BuildsGroup.Controls.Add(this.BuildsBox);
+            this.BuildsGroup.Location = new System.Drawing.Point(12, 41);
+            this.BuildsGroup.Name = "BuildsGroup";
+            this.BuildsGroup.Size = new System.Drawing.Size(300, 52);
+            this.BuildsGroup.TabIndex = 0;
+            this.BuildsGroup.TabStop = false;
+            this.BuildsGroup.Text = "Build";
+            // 
+            // BuildsBox
+            // 
+            this.BuildsBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BuildsBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.BuildsBox.FormattingEnabled = true;
+            this.BuildsBox.Location = new System.Drawing.Point(6, 19);
+            this.BuildsBox.Name = "BuildsBox";
+            this.BuildsBox.Size = new System.Drawing.Size(288, 21);
+            this.BuildsBox.TabIndex = 0;
+            // 
+            // DataGroup
+            // 
+            this.DataGroup.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.DataGroup.Controls.Add(this.DataBox);
+            this.DataGroup.Location = new System.Drawing.Point(487, 41);
+            this.DataGroup.Name = "DataGroup";
+            this.DataGroup.Size = new System.Drawing.Size(300, 52);
+            this.DataGroup.TabIndex = 1;
+            this.DataGroup.TabStop = false;
+            this.DataGroup.Text = "Data Folder";
+            // 
+            // DataBox
+            // 
+            this.DataBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DataBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.DataBox.FormattingEnabled = true;
+            this.DataBox.Location = new System.Drawing.Point(6, 19);
+            this.DataBox.Name = "DataBox";
+            this.DataBox.Size = new System.Drawing.Size(288, 21);
+            this.DataBox.TabIndex = 0;
             // 
             // Landing
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(799, 436);
+            this.Controls.Add(this.DataGroup);
+            this.Controls.Add(this.BuildsGroup);
             this.Name = "Landing";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "LambentLight: A FiveM Server Manager";
+            this.BuildsGroup.ResumeLayout(false);
+            this.DataGroup.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox BuildsGroup;
+        private System.Windows.Forms.ComboBox BuildsBox;
+        private System.Windows.Forms.GroupBox DataGroup;
+        private System.Windows.Forms.ComboBox DataBox;
     }
 }
 
