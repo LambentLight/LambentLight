0534c6d82e72e8a9d65b87da484b3158daa3db50
Hannele Ruiz <justlemoncl@gmail.com>
2019-07-20 21:11:45 -0400

    Added some comments on the existing functions

--- LambentLight/Landing.cs
+++ LambentLight/Landing.cs
@@ -16,16 +16,22 @@ namespace LambentLight
     {
         public Landing()
         {
+            // Initialize the UI elements
             InitializeComponent();
+            // Create a new configuration for NLog
             LoggingConfiguration NewConfig = new LoggingConfiguration();
+            // Add a rule for logging to the TextBox
             NewConfig.AddRule(LogLevel.Info, LogLevel.Fatal, new TextBoxTarget() { Box = LogBox, Layout = "[${date}] [${level}] [${logger}] ${message}" });
+            // Set the already created configuration
             LogManager.Configuration = NewConfig;
+            // And filll the Builds and Data folders
             BuildManager.Fill(BuildsBox);
             DataFolderManager.Fill(DataBox);
         }
 
         private async void StartItem_Click(object sender, EventArgs e)
         {
+            // Start the build with the selected options
             await ServerManager.Start((Build)BuildsBox.SelectedItem, (DataFolder)DataBox.SelectedItem);
         }
     }
