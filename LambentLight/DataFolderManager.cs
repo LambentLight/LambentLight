e7cb293f234a175fd16c1e1f6c10092045467f68
Hannele Ruiz <justlemoncl@gmail.com>
2019-07-21 19:56:08 -0400

    Added ability to create server data folders

--- LambentLight/DataFolderManager.cs
+++ LambentLight/DataFolderManager.cs
@@ -1,8 +1,11 @@
-using NLog;
+﻿using NLog;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
+using System.Net;
 using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -58,6 +61,10 @@ namespace LambentLight
         /// The logger for our current class.
         /// </summary>
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        /// <summary>
+        /// The web client for REST calls.
+        /// </summary>
+        private static WebClient Client = new WebClient();
         /// <summary>
         /// Our current set of data folders.
         /// </summary>
@@ -122,7 +129,69 @@ namespace LambentLight
         /// <param name="name">The name of the folder.</param>
         public static async Task Create(string name)
         {
+            // Create the Data folder if it does not exists
+            if (Directory.Exists("Data"))
+            {
+                Directory.CreateDirectory("Data");
+            }
 
+            // If the text is whitespaces or null, notify the user and return
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                Logger.Warn("The path that you have entered is null or consists only of whitespaces");
+                return;
+            }
+
+            // Generate the destination path
+            string NewPath = Path.Combine("Data", name);
+
+            // If the folder specified already exists, warn the user and return
+            if (Directory.Exists(NewPath))
+            {
+                Logger.Warn("The specified folder name already exists");
+                return;
+            }
+
+            // If the user wants to download the scripts
+            if (Properties.Settings.Default.DownloadScripts)
+            {
+                // Notify the user that we are downloading the repository
+                Logger.Info("Downloading Default Scripts for the Data Folder '{0}', please wait...", name);
+
+                // Create the path for the temporary zip file
+                string ZipPath = Path.Combine("Data", "cfx-server-data.zip");
+                // Start downloading the file
+                await Client.DownloadFileTaskAsync("https://github.com/citizenfx/cfx-server-data/archive/master.zip", ZipPath);
+
+                // Wait until the file has been downloaded
+                while (Client.IsBusy)
+                {
+                    await Task.Delay(0);
+                }
+
+                // After the zip file has been downloaded, extract it
+                await Task.Run(() => ZipFile.ExtractToDirectory(ZipPath, "Data"));
+                // Then, rename it to the name specified by the user
+                Directory.Move(Path.Combine("Data", "cfx-server-data-master"), NewPath);
+                // Finally, delete the temporary file
+                File.Delete(ZipPath);
+            }
+            else
+            {
+                // Create the directory and notify the user
+                Directory.CreateDirectory(NewPath);
+            }
+
+            // If the user wants to generate the configuration
+            if (Properties.Settings.Default.CreateConfig)
+            {
+                // Notify the user that we have finished with the creation of the folder
+                Logger.Info("The Data Folder '{0}' has been created", name);
+
+                // Get the string and add a random RCON password to it
+                string Config = string.Format(Encoding.UTF8.GetString(Properties.Resources.server_cfg), GenerateSecureString(32));
+                File.WriteAllText(Path.Combine(NewPath, "server.cfg"), Config);
+            }
         }
 
         /// <summary>
