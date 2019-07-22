1a8bd05eceef7c6c4b1a8e87bf2e4031bdab7974
Hannele Ruiz <justlemoncl@gmail.com>
2019-07-22 10:59:27 -0400

    Added enums for Resource and Compression Types

--- LambentLight/ResourceManager.cs
+++ LambentLight/ResourceManager.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +7,44 @@ using System.Threading.Tasks;
 
 namespace LambentLight
 {
+    /// <summary>
+    /// Identifies the type of a resource.
+    /// </summary>
+    public enum ResourceType
+    {
+        /// <summary>
+        /// A simple script.
+        /// </summary>
+        Script = 0,
+        /// <summary>
+        /// A completely different gamemode.
+        /// </summary>
+        GameMode = 1,
+        /// <summary>
+        /// A new loading screen.
+        /// </summary>
+        LoadingScreen = 2
+    }
+
+    /// <summary>
+    /// The type of compression used for the 
+    /// </summary>
+    public enum CompressionType
+    {
+        /// <summary>
+        /// The classic ZIP file.
+        /// </summary>
+        Zip = 0,
+        /// <summary>
+        /// 7zip or Seven ZIP.
+        /// </summary>
+        SevenZip = 1,
+        /// <summary>
+        /// RAR from WinRAR.
+        /// </summary>
+        Rar = 2
+    }
+
     /// <summary>
     /// Class that contains the informatioon of a specific downloadable resource.
     /// </summary>
