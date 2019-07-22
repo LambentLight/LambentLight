e653980f283141532624a05c2f832c8dd9974473
Hannele Ruiz <justlemoncl@gmail.com>
2019-07-22 09:58:26 -0400

    Added basics for the resource manager

---
+++ LambentLight/ResourceManager.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambentLight
+{
+    /// <summary>
+    /// Class that contains the informatioon of a specific downloadable resource.
+    /// </summary>
+    public class Resource
+    {
+    }
+
+    /// <summary>
+    /// Class that manages the resources of the installer.
+    /// </summary>
+    public static class ResourceManager
+    {
+        /// <summary>
+        /// All of the resources that are available for installing.
+        /// </summary>
+        public static List<Resource> Resources = new List<Resource>();
+    }
+}
