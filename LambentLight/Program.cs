8ef366b8f6fa065a6679901309cfdeaf43e01b0b
Hannele Ruiz <justlemoncl@gmail.com>
2019-07-29 11:50:39 -0400

    Added some comments on the Program class

--- LambentLight/Program.cs
+++ LambentLight/Program.cs
@@ -3,7 +3,7 @@ using System.Windows.Forms;
 
 namespace LambentLight
 {
-    static class Program
+    public static class Program
     {
         /// <summary>
         /// The main entry point for the application.
@@ -11,14 +11,21 @@ namespace LambentLight
         [STAThread]
         static void Main()
         {
+            // If a settings upgrade is required
             if (Properties.Settings.Default.UpgradeRequired)
             {
+                // Upgrade the settings
                 Properties.Settings.Default.Upgrade();
+                // Store a bool so we know that we have upgraded
                 Properties.Settings.Default.UpgradeRequired = false;
+                // And save the new settings
                 Properties.Settings.Default.Save();
             }
+            // Enable the visual styles for the application
             Application.EnableVisualStyles();
+            // Use the classic rendering for compatibility
             Application.SetCompatibleTextRenderingDefault(false);
+            // And run the application with our main form
             Application.Run(new Landing());
         }
     }
