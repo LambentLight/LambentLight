1b2c9c3a4f2929f5039ac9b72664188077cc3b50
Hannele Ruiz <justlemoncl@gmail.com>
2019-08-08 03:46:47 -0400

    Added note for when users are trying to run LambentLight on macOS

--- LambentLight/Program.cs
+++ LambentLight/Program.cs
@@ -15,7 +15,7 @@ namespace LambentLight
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static int Main()
         {
             // If a settings upgrade is required
             if (Properties.Settings.Default.UpgradeRequired)
@@ -31,12 +31,27 @@ namespace LambentLight
             Application.EnableVisualStyles();
             // Use the classic rendering for compatibility
             Application.SetCompatibleTextRenderingDefault(false);
+            MessageBox.Show(Locations.Absolute);
+            // If the program is running on macOS, notify the user and exit
+            if (Checks.IsMacOS)
+            {
+                MessageBox.Show("macOS is not supported by either LambentLight or FiveM!\nThe application will now close.");
+                return 2;
+            }
+            // If we are running Linux, tell the user that only Ubuntu has been tested
+            if (Checks.IsLinux)
+            {
+                MessageBox.Show("Thanks for using LambentLight on Linux!\nPlease note that the only distro that has been tested is Ubuntu.\nIf you find any bugs, please report them. Thanks!");
+            }
+
             // Create our main form
             Form = new Landing();
             // Prepare the downloader for our operations
             Downloader.Prepare();
             // And run the application with our main form
             Application.Run(Form);
+            // Once we are back, return a code 0
+            return 0;
         }
     }
 }
