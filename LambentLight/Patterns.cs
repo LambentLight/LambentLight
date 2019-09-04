4fed4bcfa18e0476a1664363f7f42a34079ff662
Hannele Ruiz <justlemoncl@gmail.com>
2019-09-04 01:03:28 -0400

    Added RegEx pattern for finding resources in configuration files

---
+++ LambentLight/Patterns.cs
@@ -0,0 +1,13 @@
+﻿namespace LambentLight
+{
+    /// <summary>
+    /// Regular Expression patterns.
+    /// </summary>
+    public static class Patterns
+    {
+        /// <summary>
+        /// Pattern for finding a resource inside a configuration file.
+        /// </summary>
+        public static string Resource = "start {0}*\n";
+    }
+}
