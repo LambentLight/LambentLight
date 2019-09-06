d5b81fa4c8429bafddcb40b24b503dffb244f919
Hannele Ruiz <justlemoncl@gmail.com>
2019-09-06 03:58:34 -0400

    Added boolean to know if the Data Folder should be created or not

--- LambentLight/Creator.cs
+++ LambentLight/Creator.cs
@@ -12,6 +12,11 @@ namespace LambentLight
 {
     public partial class Creator : Form
     {
+        /// <summary>
+        /// If the Data Folder should be created after exiting the form.
+        /// </summary>
+        public bool ShouldBeCreated { get; set; } = false;
+
         public Creator()
         {
             // Initialize the UI elements
@@ -20,7 +25,10 @@ namespace LambentLight
 
         private void CreateButton_Click(object sender, EventArgs e)
         {
-
+            // Save that the Data Folder should be created
+            ShouldBeCreated = true;
+            // And close the form
+            Close();
         }
 
         private void SaveButton_Click(object sender, EventArgs e)
