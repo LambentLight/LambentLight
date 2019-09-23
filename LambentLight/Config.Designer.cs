68570467f5e7600fd19d162c0065b19284f70b1f
Hannele Ruiz <justlemoncl@gmail.com>
2019-09-23 16:47:06 -0300

    Added basic controls for the settings window

--- LambentLight/Config.Designer.cs
+++ LambentLight/Config.Designer.cs
@@ -29,13 +29,48 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Config));
+            this.ConfigTabControl = new System.Windows.Forms.TabControl();
+            this.BasicTabPage = new System.Windows.Forms.TabPage();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.ConfigTabControl.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // ConfigTabControl
+            // 
+            this.ConfigTabControl.Controls.Add(this.BasicTabPage);
+            this.ConfigTabControl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ConfigTabControl.Location = new System.Drawing.Point(0, 0);
+            this.ConfigTabControl.Name = "ConfigTabControl";
+            this.ConfigTabControl.SelectedIndex = 0;
+            this.ConfigTabControl.Size = new System.Drawing.Size(384, 420);
+            this.ConfigTabControl.TabIndex = 0;
+            // 
+            // BasicTabPage
+            // 
+            this.BasicTabPage.Location = new System.Drawing.Point(4, 22);
+            this.BasicTabPage.Name = "BasicTabPage";
+            this.BasicTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.BasicTabPage.Size = new System.Drawing.Size(376, 394);
+            this.BasicTabPage.TabIndex = 0;
+            this.BasicTabPage.Text = "Basic";
+            this.BasicTabPage.UseVisualStyleBackColor = true;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(297, 426);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(75, 23);
+            this.CloseButton.TabIndex = 1;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
             // 
             // Config
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(384, 461);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.ConfigTabControl);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.HelpButton = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -44,10 +79,15 @@
             this.Name = "Config";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Settings";
+            this.ConfigTabControl.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl ConfigTabControl;
+        private System.Windows.Forms.TabPage BasicTabPage;
+        private System.Windows.Forms.Button CloseButton;
     }
 }
