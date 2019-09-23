2088f885eb22afa868df0d8eea9e11c4d5f2b9ad
Hannele Ruiz <justlemoncl@gmail.com>
2019-09-23 16:44:14 -0300

    Made the settings window slightly bigger

--- LambentLight/Config.Designer.cs
+++ LambentLight/Config.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 411);
+            this.ClientSize = new System.Drawing.Size(384, 461);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.HelpButton = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
