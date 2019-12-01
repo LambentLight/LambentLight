eca93abeef4fce93327d64fce8df15ee844d1ac4
Hannele Ruiz <justlemoncl@gmail.com>
2019-11-30 23:48:46 -0300

    Added files for using JSON configuration files

---
+++ LambentLight/Config/AutoRestart.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace LambentLight.Config
+{
+    /// <summary>
+    /// The configuration of the Auto Restart system.
+    /// </summary>
+    public class AutoRestart
+    {
+        [JsonProperty("cron_enabled")]
+        public bool Cron { get; set; } = false;
+        [JsonProperty("cron_time")]
+        public TimeSpan CronTime { get; set; } = TimeSpan.Zero;
+        [JsonProperty("daily_enabled")]
+        public bool Daily { get; set; } = false;
+        [JsonProperty("daily_time")]
+        public TimeSpan DailyTime { get; set; } = TimeSpan.Zero;
+    }
+}
---
+++ LambentLight/Config/Builds.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace LambentLight.Config
+{
+    /// <summary>
+    /// The configuration item for storing the location of the builds.
+    /// </summary>
+    public class Builds
+    {
+        [JsonProperty("windows")]
+        public string Windows { get; set; } = "https://raw.githubusercontent.com/LambentLight/Metadata/master/builds/fivem.json";
+    }
+}
