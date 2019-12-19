7003af0ce899aeb0bc826873fcb08e81bdb88f6c
Hannele Ruiz <justlemoncl@gmail.com>
2019-12-19 00:42:18 -0300

    Added object for the player information

---
+++ LambentLight/API/CitizenPlayer.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace LambentLight.API
+{
+    /// <summary>
+    /// Player information returned by the CitizenFX Server.
+    /// </summary>
+    public class CitizenPlayer
+    {
+        [JsonProperty("endpoint")]
+        public string Endpoint { get; set; }
+        [JsonProperty("id")]
+        public uint ID { get; set; }
+        [JsonProperty("identifiers")]
+        public List<string> Identifiers { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("ping")]
+        public uint Ping { get; set; }
+    }
+}
