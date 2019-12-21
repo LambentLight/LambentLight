8bcb59caab41a38da15e773b8f773f331d468ff3
Hannele Ruiz <justlemoncl@gmail.com>
2019-12-21 19:01:56 -0300

    Added regions for the InstallInfo class

--- LambentLight/Managers/Resources/InstallInfo.cs
+++ LambentLight/Managers/Resources/InstallInfo.cs
@@ -7,10 +7,14 @@ namespace LambentLight.Managers.Resources
     /// </summary>
     public class InstallInfo
     {
+        #region Public Properties
+
         /// <summary>
         /// The folder where the resource should be installed.
         /// </summary>
         [JsonProperty("destination", Required = Required.Always)]
         public string Destination { get; set; }
+
+        #endregion
     }
 }
