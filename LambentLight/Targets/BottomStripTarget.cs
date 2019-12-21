25321c0c0f023922dba64c911340ddd1e7fe63b4
Hannele Ruiz <justlemoncl@gmail.com>
2019-12-20 22:41:16 -0300

    Make sure that the form exists and there is a handle before invoking

--- LambentLight/Targets/BottomStripTarget.cs
+++ LambentLight/Targets/BottomStripTarget.cs
@@ -13,6 +13,15 @@ namespace LambentLight.Targets
         /// <param name="LogEvent">The log information.</param>
         protected override void Write(LogEventInfo LogEvent)
         {
+            // If there is no form, return
+            if (Program.Form == null)
+            {
+                return;
+            }
+
+            // Get the handle to make sure that is available
+            IntPtr pointer = Program.Form.Handle;
+            // And change the text via Invoke
             Program.Form.Invoke(new Action(() => Program.Form.BottomToolStripStatusLabel.Text = Layout.Render(LogEvent)));
         }
     }
--- LambentLight/Targets/TextBoxTarget.cs
+++ LambentLight/Targets/TextBoxTarget.cs
@@ -13,6 +13,15 @@ namespace LambentLight.Targets
         /// <param name="LogEvent">The log information.</param>
         protected override void Write(LogEventInfo LogEvent)
         {
+            // If there is no form, return
+            if (Program.Form == null)
+            {
+                return;
+            }
+
+            // Get the handle to make sure that is available
+            IntPtr pointer = Program.Form.Handle;
+            // And add the text via Invoke
             Program.Form.Invoke(new Action(() => Program.Form.ConsoleTextBox.AppendText(Layout.Render(LogEvent) + Environment.NewLine)));
         }
     }
