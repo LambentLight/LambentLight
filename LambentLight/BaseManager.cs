09669dff24e28838521e25cf030458b82b56e74d
Hannele Ruiz <justlemoncl@gmail.com>
2020-04-24 19:55:56 -0400

    Added base class for all of the managers

---
+++ LambentLight/BaseManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+
+namespace LambentLight
+{
+    /// <summary>
+    /// Base for all of the content managers.
+    /// </summary>
+    public abstract class BaseManager
+    {
+        #region Properties
+
+        /// <summary>
+        /// If this manager is ready to work.
+        /// </summary>
+        public bool ReadyToWork { get; private set; } = false;
+
+        #endregion
+
+        #region Public Functions
+
+        /// <summary>
+        /// Initializes the current manager
+        /// </summary>
+        /// <returns></returns>
+        public async Task Initialize()
+        {
+            // If the cache could not be loaded
+            if (!LoadCache())
+            {
+                // Update the items
+                await Update();
+            }
+            // And say that this manager is ready to work
+            ReadyToWork = true;
+        }
+        /// <summary>
+        /// Updates the information of this manager.
+        /// </summary>
+        public abstract Task Update();
+        /// <summary>
+        /// Loads the existing items from the cache (if present).
+        /// </summary>
+        public abstract bool LoadCache();
+        /// <summary>
+        /// Saves the existing items onto the cache directory.
+        /// </summary>
+        public abstract void SaveCache();
+
+        #endregion
+    }
+}
