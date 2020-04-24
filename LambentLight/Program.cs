39cfcc9a7f9b63977af86cd1a5e3fba597badbd7
Hannele Ruiz <justlemoncl@gmail.com>
2020-04-24 16:29:28 -0400

    Added basic landing form

---
+++ LambentLight/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LambentLight
+{
+    /// <summary>
+    /// Class that stores the basics of the application.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The Landing Screen for the program.
+        /// </summary>
+        public static FormLanding Landing { get; private set; } = null;
+
+        /// <summary>
+        /// Main Entry Point of the application.
+        /// </summary>
+        /// <returns>The exit code of the app.</returns>
+        [STAThread]
+        public static int Main()
+        {
+            // Enable the Vista+ visual styles and GDI+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // Create the forms
+            Landing = new FormLanding();
+            // And run the app with the default form
+            Application.Run(Landing);
+            return 0;
+        }
+    }
+}
