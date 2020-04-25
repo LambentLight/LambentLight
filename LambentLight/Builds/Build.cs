6c8aeed00fe782bee82d50b223d4c9fcb86ef782
Hannele Ruiz <justlemoncl@gmail.com>
2020-04-25 12:01:50 -0400

    Added classes for storing the Build information

---
+++ LambentLight/Builds/Build.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+
+namespace LambentLight.Builds
+{
+    /// <summary>
+    /// The Target Operating System for a build.
+    /// </summary>
+    public enum TargetOS
+    {
+        Windows = 0,
+        Linux = 1
+    }
+
+    /// <summary>
+    /// Object that stores the information of a build.
+    /// </summary>
+    public class Build
+    {
+        /// <summary>
+        /// The name or identifier of the build.
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        /// <summary>
+        /// The Download URL of the build.
+        /// </summary>
+        [JsonProperty("download")]
+        public string Download { get; set; }
+        /// <summary>
+        /// Target Operating System for the build.
+        /// </summary>
+        [JsonProperty("target")]
+        public TargetOS Target { get; set; }
+    }
+}
---
+++ LambentLight/Builds/Builds.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace LambentLight.Builds
+{
+    /// <summary>
+    /// Base class for the list of builds.
+    /// </summary>
+    public class Builds
+    {
+        /// <summary>
+        /// The schema used to validate the list of builds.
+        /// </summary>
+        [JsonProperty("$schema")]
+        public string Schema { get; set; }
+        /// <summary>
+        /// The builds that are available for download.
+        /// </summary>
+        [JsonProperty("builds")]
+        public List<Build> RemoteBuilds { get; set; }
+    }
+}
