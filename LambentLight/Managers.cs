7a08b8d61b2814db312d9cb3547d789d7549f219
Hannele Ruiz <justlemoncl@gmail.com>
2020-04-25 12:05:31 -0400

    Added class for accessing all of the Managers

---
+++ LambentLight/Managers.cs
@@ -0,0 +1,15 @@
+﻿using LambentLight.Builds;
+
+namespace LambentLight
+{
+    /// <summary>
+    /// Class to access all of the content managers.
+    /// </summary>
+    public static class Managers
+    {
+        /// <summary>
+        /// Manager for the CFX Builds.
+        /// </summary>
+        public static BuildManager BuildManager = new BuildManager();
+    }
+}
