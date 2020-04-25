68c7a757bd68cf60dd33851d4eb55dfd4bc2474c
Hannele Ruiz <justlemoncl@gmail.com>
2020-04-25 12:03:30 -0400

    Added build manager

---
+++ LambentLight/Builds/BuildManager.cs
@@ -0,0 +1,84 @@
+﻿using Flurl.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace LambentLight.Builds
+{
+    /// <summary>
+    /// Manager for builds.
+    /// </summary>
+    public class BuildManager : BaseManager
+    {
+        #region Fields
+
+        /// <summary>
+        /// The location of the cache file.
+        /// </summary>
+        private readonly string cacheFile = Path.Combine("cache", "builds.json");
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// The builds available for download.
+        /// </summary>
+        public List<Build> Builds { get; private set; } = new List<Build>();
+
+        #endregion
+
+        #region Public Functions
+
+        /// <summary>
+        /// Updates the cache from the GitHub repository.
+        /// </summary>
+        public override async Task Update()
+        {
+            // Get the builds from the repository
+            Builds builds = await Program.Config.DownloadBuilds.GetJsonAsync<Builds>();
+            // Save them for later use
+            Builds = builds.RemoteBuilds;
+            // And save them to the cache
+            SaveCache();
+        }
+        /// <summary>
+        /// Loads the builds from the cache if they have been written recently.
+        /// </summary>
+        /// <returns>True if they were loaded from the drive, false otherwise.</returns>
+        public override bool LoadCache()
+        {
+            // If the file does not exists or is old, return
+            if (!File.Exists(cacheFile) || File.GetLastWriteTimeUtc(cacheFile) < (DateTime.UtcNow + TimeSpan.FromHours(6)))
+            {
+                return false;
+            }
+
+            // Otherwise, try to load it
+            try
+            {
+                string cache = File.ReadAllText(cacheFile);
+                Builds = JsonConvert.DeserializeObject<List<Build>>(cache);
+                return true;
+            }
+            catch (JsonReaderException)
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Saves the builds into the drive for later use.
+        /// </summary>
+        public override void SaveCache()
+        {
+            // Create the cache directory if it does not exists
+            Directory.CreateDirectory("cache");
+            // Just write the entire list of builds into the cache
+            File.WriteAllText(cacheFile, JsonConvert.SerializeObject(Builds));
+        }
+
+        #endregion
+    }
+}
