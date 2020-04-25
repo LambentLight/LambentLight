1976c542e11eb232dc29a42990bb17be9ed7c850
Hannele Ruiz <justlemoncl@gmail.com>
2020-04-25 14:52:48 -0400

    Added enum for selecting a game

---
+++ LambentLight/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace LambentLight
+{
+    /// <summary>
+    /// The games that are considered valid by CFX.
+    /// </summary>
+    public enum Game
+    {
+        GrandTheftAutoV = 0,
+        RedDeadRedemption2 = 1
+    }
+}
