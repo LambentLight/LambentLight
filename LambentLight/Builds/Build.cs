106f81bad7e94682e737730d19f67ed5179e7a9d
Hannele Ruiz <justlemoncl@gmail.com>
2020-04-26 08:43:14 -0400

    Added additional properties for Build objects

--- LambentLight/Builds/Build.cs
+++ LambentLight/Builds/Build.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.IO;
 
 namespace LambentLight.Builds
 {
@@ -17,6 +18,11 @@ namespace LambentLight.Builds
     public class Build
     {
         /// <summary>
+        /// Temporary location for the download of files.
+        /// </summary>
+        private readonly string tempPath = Path.Combine(Path.GetTempPath(), "LambentLight");
+
+        /// <summary>
         /// The name or identifier of the build.
         /// </summary>
         [JsonProperty("name")]
@@ -25,11 +31,26 @@ namespace LambentLight.Builds
         /// The Download URL of the build.
         /// </summary>
         [JsonProperty("download")]
-        public string Download { get; set; }
+        public string DownloadURL { get; set; }
         /// <summary>
         /// Target Operating System for the build.
         /// </summary>
         [JsonProperty("target")]
         public TargetOS Target { get; set; }
+        /// <summary>
+        /// The location of the build in the drive.
+        /// </summary>
+        [JsonIgnore]
+        public string Folder => Path.Combine("builds", "windows", Name);
+        /// <summary>
+        /// Path of the executable of the Build.
+        /// </summary>
+        [JsonIgnore]
+        public string Executable => Path.Combine(Folder, "FXServer.exe");
+        /// <summary>
+        /// If this build is installed or not.
+        /// </summary>
+        [JsonIgnore]
+        public bool IsInstalled => File.Exists(Executable);
     }
 }
