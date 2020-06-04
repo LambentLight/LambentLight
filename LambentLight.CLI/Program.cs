17b6a46272a227c9934a2861c644f99d972822ae
Hannele Ruiz <justlemoncl@gmail.com>
2020-06-04 01:01:30 -0400

    Added CLI project

---
+++ LambentLight.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LambentLight.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
