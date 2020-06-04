ca74c3ebf5dda5cb9599ecd46d35a7edee642d1d
Hannele Ruiz <justlemoncl@gmail.com>
2020-06-04 04:56:10 -0400

    Added basic Serilog logging

--- LambentLight.CLI/Program.cs
+++ LambentLight.CLI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CommandLine;
+using Serilog;
 using System.Threading;
 using WatsonWebsocket;
 
@@ -14,9 +15,16 @@ namespace LambentLight.CLI
 
         public static void Run(Arguments args)
         {
+            // Configure the Serilog Logging
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .CreateLogger();
+
             // If we need to start in daemon mode, run the websocket server
             if (args.Daemon)
             {
+                Log.Information("Starting LambentLight in Daemon Mode on {0}:{1}", args.Address, args.Port);
                 WatsonWsServer server = new WatsonWsServer(args.Address, args.Port, args.SSL);
                 server.Start();
                 Thread.Sleep(Timeout.Infinite);
