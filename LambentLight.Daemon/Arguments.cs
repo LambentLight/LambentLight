9f1708afe2164ff0c8e2d3d56b795f22f4f0c7c3
Hannele Ruiz <justlemoncl@gmail.com>
2020-06-05 04:02:44 -0400

    Added summary for the command line arguments

--- LambentLight.Daemon/Arguments.cs
+++ LambentLight.Daemon/Arguments.cs
@@ -18,14 +18,29 @@ namespace LambentLight.Daemon
             "/var/lib/lambentlight";
 #endif
 
+        /// <summary>
+        /// The IP that the WebSocket will be binded to.
+        /// </summary>
         [Option('i', "ip", Default = "127.0.0.1", HelpText = "IP Address to bind.")]
         public string IP { get; set; }
+        /// <summary>
+        /// The port that the WebSocket will be binded to.
+        /// </summary>
         [Option('p', "port", Default = 7013, HelpText = "Port to bind.")]
         public int Port { get; set; }
+        /// <summary>
+        /// The working directory of the program.
+        /// </summary>
         [Option('d', "dir", Default = directory, HelpText = "The directory used to store Builds and Data Folders.")]
         public string Directory { get; set; }
+        /// <summary>
+        /// If SSL should be used for the WebSocket.
+        /// </summary>
         [Option('s', "ssl", Default = false, HelpText = "If SSL should be used or not.")]
         public bool SSL { get; set; }
+        /// <summary>
+        /// The IPs that are authorized to make WebSocket requests.
+        /// </summary>
         [Option('a', "authorized", Required = false, HelpText = "IPs that are allowed to connect.")]
         public IEnumerable<string> Allowed { get; set; } = new List<string> { "127.0.0.1" };
     }
