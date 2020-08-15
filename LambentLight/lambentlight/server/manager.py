b1f931ee445768909f3962a2f9e3bf92c700e519
Hannele Ruiz <justlemoncl@gmail.com>
2020-08-15 18:18:57 -0400

    Added configuration loading on the Manager

--- LambentLight/lambentlight/server/manager.py
+++ LambentLight/lambentlight/server/manager.py
@@ -1,7 +1,18 @@
+import json
+from os.path import join
+
+from .arguments import arguments
+
+
 class Manager:
     """
     The main manager of LambentLight.
     """
+    def __init__(self):
+        # Load the configuration of the program
+        self.config_path = join(arguments.work_dir, "Config.json")
+        with open(self.config_path) as file:
+            self.config = json.load(file)
 
 
 manager = Manager()
