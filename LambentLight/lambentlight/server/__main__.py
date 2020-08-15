da9697eb93e2670db89e2beee81930d74f27bede
Hannele Ruiz <justlemoncl@gmail.com>
2020-08-15 15:09:09 -0400

    Added simple argument parsing

---
+++ LambentLight/lambentlight/server/__main__.py
@@ -0,0 +1,9 @@
+from .arguments import parse_arguments
+
+
+def main():
+    arguments = parse_arguments()
+
+
+if __name__ == "__main__":
+    main()
---
+++ LambentLight/lambentlight/server/arguments.py
@@ -0,0 +1,20 @@
+import argparse
+from platform import system
+
+
+default_work_dir = "C:\\ProgramData\\LambentLight" if system() == "Windows" else "/var/lib/LambentLight"
+
+
+def parse_arguments():
+    """
+    Parses the launch arguments.
+    """
+    # Create the parser
+    parser = argparse.ArgumentParser(description="Daemon for managing your CFX Servers.",
+                                     epilog="Check https://justalemon.ml/LambentLight for more info.",
+                                     add_help=True, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    # Add the arguments
+    parser.add_argument("-w", "--work-dir", dest="work_dir", default=default_work_dir,
+                        help="folder used to store the server data")
+    # And return them parsed
+    return parser.parse_args()
