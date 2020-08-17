84c0808441de82d8353ce2330ae5a848e774b454
Hannele Ruiz <justlemoncl@gmail.com>
2020-08-17 13:03:26 -0400

    Added support for downloading builds

--- LambentLight/lambentlight/server/build.py
+++ LambentLight/lambentlight/server/build.py
@@ -1,13 +1,24 @@
+import logging
+import os
 import os.path
+import shutil
+import tempfile
 
 from .arguments import arguments
 from .checks import is_ubuntu, is_windows
+from .compression import extract
+
+import aiohttp
+
+logger = logging.getLogger("lambentlight")
 
 
 class Build:
     """
     The information of a CFX Build.
     """
+    builds_dir: str = os.path.join(arguments.work_dir, "builds")
+
     def __init__(self, *, folder: str = None, download: str = None, name: str = None):
         # If the parameters are mixed in an invalid format, raise an exception
         if folder and (download or name):
@@ -17,11 +28,11 @@ class Build:
         if folder:
             self.folder = folder
             self.name = os.path.dirname(folder)
-            self.download = None
+            self.url = None
         else:
             self.folder = os.path.join(arguments.work_dir, "builds", name)
             self.name = name
-            self.download = download
+            self.url = download
 
     @property
     def is_ready(self):
@@ -35,3 +46,39 @@ class Build:
             return os.path.isfile(os.path.join(self.folder, "run.sh"))
         else:
             return False
+
+    async def download(self, session: aiohttp.ClientSession):
+        """
+        Downloads the Build.
+
+        If is already installed, it will be removed and re-downloaded.
+        """
+        # If there is no download link, return
+        if not self.url:
+            return False
+
+        # Otherwise, make the required paths and create them if needed
+        temp_dir = os.path.join(tempfile.gettempdir(), "lambentlight", "builds")
+        os.makedirs(temp_dir, exist_ok=True)
+        temp_file = os.path.join(temp_dir, self.name + "_dl")
+        logger.info(f"Downloading build {self.name} from {self.url} to {temp_file}")
+        # Then, request it and save it in chunks
+        async with session.get(self.url) as resp:
+            with open(temp_file, "wb") as file:
+                while True:
+                    chunk = await resp.content.read(1024 * 1024)
+                    if not chunk:
+                        break
+                    file.write(chunk)
+        # Time to extract it!
+        # Make the paths and extract it
+        ext_path = os.path.join(tempfile.gettempdir(), "lambentlight", "builds", self.name)
+        extracted = extract(temp_file, ext_path)
+        # If we were unable to extract the file, log a message
+        if not extracted:
+            logger.error(f"Installation of Build {self.name} failed: Unable to Extract")
+            return False
+        # Finish by moving the directory to the target and notifying it
+        os.makedirs(self.builds_dir)
+        shutil.move(ext_path, self.builds_dir)
+        return True
--- LambentLight/lambentlight/server/web.py
+++ LambentLight/lambentlight/server/web.py
@@ -1,3 +1,5 @@
+import json
+
 from aiohttp import web
 
 from .manager import manager
@@ -39,6 +41,38 @@ async def builds(request):
         "Cache-Control": f"max-age={60 * 60}"  # 1 hour
     }
     return web.json_response(blist, headers=headers)
+
+
+@routes.post("/builds/download")
+async def download_build(request: web.Request):
+    """
+    Downloads a specific build.
+    """
+    # Try to get the request as JSON
+    try:
+        data = await request.json()
+    except json.JSONDecodeError:
+        return web.json_response({"message": "Body is not JSON or is malformed."}, status=400)
+    # If the request does not has an ID, return a 400
+    if "name" not in data:
+        return web.json_response({"message": "No Build name was specified."}, status=400)
+    # Try to get the builds that match the ID
+    matches = [x for x in manager.builds if x.name == data["name"]]
+    # If no matches were found, return a 404
+    if not matches:
+        return web.json_response({"message": "No Builds were found with that name"}, status=404)
+    # Get the build that we plan to manage
+    build = matches[0]
+    # If is already ready to be used and is not forced, return
+    if build.is_ready and not data.get("force", False):
+        return web.json_response({"message": "Build is already Downloaded and Ready."}, status=409)
+    # Otherwise, start the download and notify if it was a success
+    else:
+        success = await build.download(manager.session)
+        if success:
+            return web.json_response({"message": "Build was downloaded"})
+        else:
+            return web.json_response({"messages": "Error when downloading the build."}, status=500)
 
 
 app.add_routes(routes)
