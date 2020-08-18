65d212d1ca171daabc875818d3c8cc516013f07b
Hannele Ruiz <justlemoncl@gmail.com>
2020-08-18 13:41:04 -0400

    Added global CFX Token Parameter

--- LambentLight/lambentlight/server/manager.py
+++ LambentLight/lambentlight/server/manager.py
@@ -16,6 +16,7 @@ from .checks import is_ubuntu, is_windows
 logger = logging.getLogger("lambentlight")
 default = {
     "token_api": "".join(secrets.choice(string.ascii_letters + string.digits) for _ in range(32)),
+    "token_cfx": "",
     "dl_builds": "https://raw.githubusercontent.com/LambentLight/Builds/master/builds.json"
 }
 
