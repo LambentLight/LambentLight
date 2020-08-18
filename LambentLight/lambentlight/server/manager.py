d127b1b33394e2d14190d30fa86f24279951f5bd
Hannele Ruiz <justlemoncl@gmail.com>
2020-08-18 13:02:22 -0400

    Renamed token configuration option to token_api

--- LambentLight/lambentlight/server/manager.py
+++ LambentLight/lambentlight/server/manager.py
@@ -15,7 +15,7 @@ from .checks import is_ubuntu, is_windows
 
 logger = logging.getLogger("lambentlight")
 default = {
-    "token": "".join(secrets.choice(string.ascii_letters + string.digits) for _ in range(32)),
+    "token_api": "".join(secrets.choice(string.ascii_letters + string.digits) for _ in range(32)),
     "dl_builds": "https://raw.githubusercontent.com/LambentLight/Builds/master/builds.json"
 }
 
