c904fe41159a477f80e3ef0d18f302eceaf370e6
Hannele Ruiz <justlemoncl@gmail.com>
2020-08-18 13:08:19 -0400

    Added per-data folder configuration

--- LambentLight/lambentlight/server/datafolder.py
+++ LambentLight/lambentlight/server/datafolder.py
@@ -1,4 +1,13 @@
+import json
+import logging
 import os.path
+
+logger = logging.getLogger("lambentlight")
+default = {
+    "token_cfx": "",
+    "token_steam": "",
+    "game": "gtav"
+}
 
 
 class DataFolder:
@@ -9,6 +18,8 @@ class DataFolder:
         self.path = os.path.abspath(path)
         self.name = os.path.basename(self.path)
         self.local = local
+        self.config = None
+        self.reload_configuration()
 
     @property
     def can_be_used(self):
@@ -16,3 +27,25 @@ class DataFolder:
         Checks if the Data Folder can be used for a server.
         """
         return os.path.isdir(self.path)
+
+    def reload_configuration(self):
+        """
+        Reloads the configuration specific to this Data Folder.
+        """
+        path = os.path.join(self.path, "lambentlight.json")
+
+        # If the file is there, load it
+        if os.path.isfile(path):
+            newconfig = {}
+            with open(path) as file:
+                loaded = json.load(file)
+                for key, item in default.items():
+                    if key in loaded:
+                        newconfig[key] = loaded[key]
+                    else:
+                        newconfig[key] = item
+                self.config = newconfig
+        # Otherwise, use the default values
+        else:
+            logger.warning(f"Data Folder {self.name} does not has a LambentLight Configuration File")
+            self.config = default
