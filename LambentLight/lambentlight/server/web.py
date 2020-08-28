e351938cf69b6d4dc5806165a1dba893c140bbac
Hannele Ruiz <justlemoncl@gmail.com>
2020-08-28 06:53:09 -0400

    Improved endpoint for specific builds

--- LambentLight/lambentlight/server/web.py
+++ LambentLight/lambentlight/server/web.py
@@ -90,36 +90,44 @@ class BuildsView(web.View):
         return web.json_response({"count": len(manager.builds)})
 
 
-@routes.post("/builds/download")
-async def download_build(request: web.Request):
+@routes.view("/builds/{name}")
+class BuildView(web.View):
     """
-    Downloads a specific build.
+    Route to manage specific builds.
     """
-    # Try to get the request as JSON
-    try:
-        data = await request.json()
-    except json.JSONDecodeError:
-        return web.json_response({"message": "Body is not JSON or is malformed."}, status=400)
-    # If the request does not has an ID, return a 400
-    if "name" not in data:
-        return web.json_response({"message": "No Build name was specified."}, status=400)
-    # Try to get the builds that match the ID
-    matches = [x for x in manager.builds if x.name == data["name"]]
-    # If no matches were found, return a 404
-    if not matches:
-        return web.json_response({"message": "No Builds were found with that name"}, status=404)
-    # Get the build that we plan to manage
-    build = matches[0]
-    # If is already ready to be used and is not forced, return
-    if build.is_ready and not data.get("force", False):
-        return web.json_response({"message": "Build is already Downloaded and Ready."}, status=409)
-    # Otherwise, start the download and notify if it was a success
-    else:
-        success = await build.download(manager.session)
-        if success:
-            return web.json_response({"message": "Build was downloaded"})
+    async def get(self):
+        """
+        Gets the information of a specific build.
+        """
+        # Try to get the builds that match the name
+        matches = [x for x in manager.builds if x.name == self.request.match_info["name"]]
+        # If none were found, show an error message
+        if not matches:
+            return web.json_response({"message": "No Builds were found with that name."}, status=404)
+        # Otherwise, return the build information
+        return web.json_response(dict(matches[0]))
+
+    async def post(self):
+        """
+        Downloads a specific build.
+        """
+        # Try to get the builds that match the name
+        matches = [x for x in manager.builds if x.name == self.request.match_info["name"]]
+        # If no matches were found, return a 404
+        if not matches:
+            return web.json_response({"message": "No Builds were found with that name."}, status=404)
+        # Get the build that we plan to manage
+        build = matches[0]
+        # If is already ready to be used and is not forced, return
+        if build.is_ready and self.request.query.get("force", "0") != "1":
+            return web.json_response({"message": "Build is already Downloaded and Ready."}, status=409)
+        # Otherwise, start the download and notify if it was a success
         else:
-            return web.json_response({"messages": "Error when downloading the build."}, status=500)
+            success = await build.download(manager.session)
+            if success:
+                return web.Response(status=204)
+            else:
+                return web.json_response({"messages": "Error when downloading the build."}, status=500)
 
 
 @routes.view("/folders")
