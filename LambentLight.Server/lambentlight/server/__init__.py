196e41a7d6bd4c6b06934415a615cfc5c1a52a87
Hannele Ruiz <justlemoncl@gmail.com>
2021-01-14 16:32:28 -0300

    Import some exceptions for easy use

--- LambentLight.Server/lambentlight/server/__init__.py
+++ LambentLight.Server/lambentlight/server/__init__.py
@@ -4,6 +4,7 @@ from .checks import is_ubuntu, is_windows, is_valid
 from .compression import CompressionType, detect, extract
 from .datafolder import DataFolder
 from .decorators import requires_build, requires_folder, requires_server
+from .exceptions import LambentLightServerException, ServerRunningException
 from .manager import manager
 from .resources import LocalResource
 from .web import app
