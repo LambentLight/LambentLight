098cc759efbb168b2a6febaf3d666c00bc453324
Hannele Ruiz <justlemoncl@gmail.com>
2021-02-08 05:33:48 -0300

    Added missing folder.py (oops)

---
+++ LambentLight.Client/lambentlight/client/folder.py
@@ -0,0 +1,25 @@
+import lambentlight.client as client
+
+
+async def create_folder(name: str, no_resources: bool):
+    """
+    Creates the specified Data Folder.
+    """
+    info = await client.put("/folders", {"name": name, "skip_resources": no_resources})
+    client.print_as_table(info, capitalize=True)
+
+
+async def get_folder(name: str):
+    """
+    Gets the information of a specific folder.
+    """
+    info = await client.get(f"/folders/{name}")
+    client.print_as_table(info, capitalize=True)
+
+
+async def get_resources(name: str):
+    """
+    Gets the resources present on a Data Folder.
+    """
+    info = await client.get(f"/folders/{name}/resources")
+    client.print_with_header(["Name", "Path"], *[[x["name"], x["path"]] for x in info])
