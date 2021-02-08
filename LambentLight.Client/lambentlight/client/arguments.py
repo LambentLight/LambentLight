bc219ff1f6469e3fe50b5837a6bd9fd550920b9d
Hannele Ruiz <justlemoncl@gmail.com>
2021-02-07 21:59:48 -0300

    Removed aliases for the arguments

--- LambentLight.Client/lambentlight/client/arguments.py
+++ LambentLight.Client/lambentlight/client/arguments.py
@@ -14,12 +14,12 @@ def parse_arguments():
     # LIST SERVERS
     server = subparsers.add_parser("servers",
                                    help="lists the Servers currently running")   # LIST BUILDS
-    builds = subparsers.add_parser("builds", aliases=["bl"],
+    builds = subparsers.add_parser("builds",
                                    help="Lists the Builds known by LambentLight")
     builds.add_argument("-U", "--update", action="store_true",
                         help="Updates the list of Builds before requesting them.")
     # MANAGE BUILDS
-    build = subparsers.add_parser("build", aliases=["b"],
+    build = subparsers.add_parser("build",
                                   help="Manages specific Builds")
     build.add_argument("build")
     build_tasks = build.add_mutually_exclusive_group()
