9a33e3c56a13e0f5420a6f230f9117932a74ccf9
Hannele Ruiz <justlemoncl@gmail.com>
2021-02-15 03:54:27 -0300

    Added existing Manager form

---
+++ LambentLight.GUI/LambentLight.GUI/FormManager.cs
@@ -0,0 +1,16 @@
+using System.Windows.Forms;
+
+namespace LambentLight
+{
+    public partial class FormManager : Form
+    {
+        #region Constructor
+
+        public FormManager(string host, string token)
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
