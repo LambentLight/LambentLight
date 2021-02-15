31152c6617c969a8be4728290d6c7bb784eb8a65
Hannele Ruiz <justlemoncl@gmail.com>
2021-02-15 12:54:08 -0300

    Fixed decorators not working after singleton removal

--- LambentLight.Server/lambentlight/server/decorators.py
+++ LambentLight.Server/lambentlight/server/decorators.py
@@ -8,7 +8,7 @@ def requires_build(func):
     Decorator that makes a route get a Build.
     """
     async def decorator(self):
-        matches = [x for x in server.manager.builds if x.name == self.request.match_info["name"]]
+        matches = [x for x in self.request.app["manager"].builds if x.name == self.request.match_info["name"]]
         if not matches:
             return web.json_response({"message": "No Builds were found with the specified Name."},
                                      status=404)
@@ -21,7 +21,7 @@ def requires_folder(func):
     Decorator that makes a route get a Data Folder.
     """
     async def decorator(self):
-        matches = [x for x in server.manager.folders if x.name == self.request.match_info["name"]]
+        matches = [x for x in self.request.app["manager"].folders if x.name == self.request.match_info["name"]]
         if not matches:
             return web.json_response({"message": "No Data Folders were found with the specified Name."},
                                      status=404)
@@ -34,7 +34,7 @@ def requires_server(func):
     Decorator that makes a route get a Server Running.
     """
     async def decorator(self):
-        matches = [x for x in server.manager.folders if x.name == self.request.match_info["name"] and x.is_running]
+        matches = [x for x in self.request.app["manager"].folders if x.name == self.request.match_info["name"] and x.is_running]
         if not matches:
             return web.json_response({"message": "No Servers Running were found with the specified Name"},
                                      status=404)
