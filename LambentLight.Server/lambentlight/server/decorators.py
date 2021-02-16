581211a0ef9a306e6d18024db882c1ea559adf9f
Hannele Ruiz <justlemoncl@gmail.com>
2021-02-16 10:01:56 -0300

    Reworked decorators to be made more readable

--- LambentLight.Server/lambentlight/server/decorators.py
+++ LambentLight.Server/lambentlight/server/decorators.py
@@ -1,18 +1,16 @@
 from aiohttp import web
 
-from lambentlight import server
-
 
 def requires_build(func):
     """
     Decorator that makes a route get a Build.
     """
     async def decorator(self):
-        matches = [x for x in self.request.app["manager"].builds if x.name == self.request.match_info["name"]]
-        if not matches:
-            return web.json_response({"message": "No Builds were found with the specified Name."},
-                                     status=404)
-        return await func(self, matches[0])
+        for build in self.request.app["manager"].builds:
+            if build.name == self.request.match_info["name"]:
+                return await func(self, build)
+        return web.json_response({"message": "No Builds Running were found with the specified Name"},
+                                 status=404)
     return decorator
 
 
@@ -21,11 +19,11 @@ def requires_folder(func):
     Decorator that makes a route get a Data Folder.
     """
     async def decorator(self):
-        matches = [x for x in self.request.app["manager"].folders if x.name == self.request.match_info["name"]]
-        if not matches:
-            return web.json_response({"message": "No Data Folders were found with the specified Name."},
-                                     status=404)
-        return await func(self, matches[0])
+        for folder in self.request.app["manager"].folders:
+            if folder.name == self.request.match_info["name"]:
+                return await func(self, folder)
+        return web.json_response({"message": "No Data Folders Running were found with the specified Name"},
+                                 status=404)
     return decorator
 
 
@@ -34,9 +32,9 @@ def requires_server(func):
     Decorator that makes a route get a Server Running.
     """
     async def decorator(self):
-        matches = [x for x in self.request.app["manager"].folders if x.name == self.request.match_info["name"] and x.is_running]
-        if not matches:
-            return web.json_response({"message": "No Servers Running were found with the specified Name"},
-                                     status=404)
-        return await func(self, matches[0])
+        for folder in self.request.app["manager"].folders:
+            if folder.name == self.request.match_info["name"] and folder.is_running:
+                return await func(self, folder)
+        return web.json_response({"message": "No Servers Running were found with the specified Name"},
+                                 status=404)
     return decorator
